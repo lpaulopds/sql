c37346f640c52c512ccba5000887bde111eae1b5
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-10-04 18:57:59 -0300

    Update.

--- index.php
+++ index.php
@@ -1,14 +1,48 @@
 <?php 
 
-$con_string = "host=postgres port=5432 dbname=testedb user=postgres password=postgres";
-$bdcon4 = pg_connect($con_string);
-// $db = pg_connect("$bdcon4");
-if(!$bdcon4)
+// $con_string = "host=postgres port=5432 dbname=loja user=postgres password=postgres";
+// $bdcon = pg_connect($con_string);
+// if(!$bdcon)
+// {
+//     echo "Error : Unable to open database\n";
+//     echo pg_last_error($bdcon);
+// }
+// else {
+//     echo "Opened database successfully\n";
+// }
+
+// $sql =<<<EOF
+//     CREATE TABLE COMPANY
+//     (ID INT PRIMARY KEY     NOT NULL,
+//     NAME           TEXT    NOT NULL,
+//     AGE            INT     NOT NULL,
+//     ADDRESS        CHAR(50),
+//     SALARY         REAL);
+// EOF;
+
+// $ret = pg_query($bdcon, $sql);
+// if(!$ret) {
+//     echo pg_last_error($bdcon);
+// } else {
+//     echo "Table created successfully\n";
+// }
+// pg_close($bdcon);
+// phpinfo();
+// die;
+require 'postgres/config.php';
+try
 {
-    echo "Error : Unable to open database\n";
-    echo pg_last_error($bdcon4);
+    $dsn = "psql:host=$host;port=$port;dbname=$db;";
+    $pdo = new PDO($dsn,$user,$password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    if ($pdo) {
+        echo "Connected with database.";
+    }
 }
-else
-{
-    echo "Opened database successfully\n";
+catch(PDOException $e) {
+    die($e->getMessage());
+}
+finally {
+    if ($pdo) {
+        $pdo = null;
+    }
 }
