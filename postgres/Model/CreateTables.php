1218c619d0d164cf9672404b57ec2d8e8aba451f
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-11-01 03:16:53 -0300

    Indentation.

--- postgres/Model/CreateTables.php
+++ postgres/Model/CreateTables.php
@@ -24,35 +24,35 @@ class CreateTables
     public function createTables()
     {
         $sqlList = [
-            'DROP TABLE departamentos',
-            'CREATE TABLE departamentos
-            (
-                departamento varchar(100),
-                divisao varchar(100),
-                primary key (departamento)
-            );',
-            'DROP TABLE localizacao',
-            'CREATE TABLE localizacao
-            (
-                idRegiao int,
-                localizacao varchar(20),
-                pais varchar(20),
-                primary key (idRegiao)
-            );',
-            'DROP TABLE funcionarios',
-            'CREATE TABLE funcionarios 
-            (
-                idFuncionario integer,
-                nome varchar(100),
-                email varchar(200),
-                sexo varchar(10),
-                departamento varchar(100),
-                admissao date,
-                salario integer,
-                cargo varchar(100),
-                idRegiao int,
-                primary key (idFuncionario)
-            );'
+            'DROP TABLE departamentos;',
+                'CREATE TABLE departamentos
+                (
+                    departamento varchar(100),
+                    divisao varchar(100),
+                    primary key (departamento)
+                );',
+            'DROP TABLE localizacao;',
+                'CREATE TABLE localizacao
+                (
+                    idRegiao int,
+                    localizacao varchar(20),
+                    pais varchar(20),
+                    primary key (idRegiao)
+                );',
+            'DROP TABLE funcionarios;',
+                'CREATE TABLE funcionarios 
+                (
+                    idFuncionario integer,
+                    nome varchar(100),
+                    email varchar(200),
+                    sexo varchar(10),
+                    departamento varchar(100),
+                    admissao date,
+                    salario integer,
+                    cargo varchar(100),
+                    idRegiao int,
+                    primary key (idFuncionario)
+                );'
         ];
 
         // execute each sql statement to create new tables
