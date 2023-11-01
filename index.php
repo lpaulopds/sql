dd4ff8e7d1a139d687f4388b99047d2970981a02
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-11-01 19:56:04 -0300

    Revert "Waiting for merge with main now."
    This reverts commit 9d7bb9404b57cfcd817c3fb513924b11fbc1e9bf.

--- index.php
+++ index.php
@@ -1,78 +1,66 @@
 <?php 
-
-// $con_string = "host=postgres port=5432 user=postgres password=postgres";
-// $bdcon = pg_connect($con_string);
-// if(!$bdcon)
-// {
-//     echo "Error : Unable to open database\n";
-// }
-// else {
-//     echo "Opened database successfully\n";
-// }
-// die;
-
-// $sql =<<<EOF
-//     CREATE TABLE COMPANY
-//     (ID INT PRIMARY KEY     NOT NULL,
-//     NAME           TEXT    NOT NULL,
-//     AGE            INT     NOT NULL,
-//     ADDRESS        CHAR(50),
-//     SALARY         REAL);
-// EOF;
-
-// $ret = pg_query($bdcon, $sql);
-// if(!$ret) {
-//     echo pg_last_error($bdcon);
-// } else {
-//     echo "Table created successfully\n";
-// }
-// pg_close($bdcon);
-
 // echo "<pre>";
 // print_r($_SERVER);
 // echo "</pre>";
 // phpinfo();
 // die;
 
-// require 'postgres/config.php';
-// try
-// {
-//     $dsn = "pgsql:host=$host;port=$port;";
-//     $pdo = new PDO($dsn,$user,$password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-//     if ($pdo) {
-//         echo "Connected with database.";
-//     }
-// }
-// catch(PDOException $e) {
-//     die($e->getMessage());
-// }
-// // finally {
-// //     if ($pdo) {
-// //         $pdo = null;
-// //     }
-// // }
-
-// $sql =<<<EOF
-//     CREATE DATABASE LOJA;
-// EOF;
-
-// $ret = pg_connect($pdo, $sql);
-// if(!$ret) {
-//     echo pg_last_error($pdo);
-// } else {
-//     echo "Table created successfully\n";
-// }
-// pg_close($pdo);
-
 require __DIR__ . '/vendor/autoload.php';
 
 use \App\Connect\Connection as Connection;
-
-try 
+try
 {
-    Connection::get()->connect();
-    print "Connected";
+    $pdo = Connection::get()->connect();
+    print "Connected" . "<br>";
 }
 catch (\PDOException $e) {
     print $e->getMessage();
+}
+
+
+// use \App\Model\CreateDatabase as CreateDatabase;
+
+// try
+// {
+//     $databaseCreator = new CreateDatabase($pdo);
+//     $database = $databaseCreator->createDB();
+
+//     $database ? print 'Created Database' : print 'Error in query create database';
+
+//     foreach ($database as $db) {
+//         print $db . '<br>';
+//     }
+// }
+// catch (\PDOException $e) {
+//     print $e->getMessage();
+// }
+
+
+use \App\Model\CreateTables as CreateTables;
+
+try
+{
+    $tablesCreator = new CreateTables($pdo);
+    $tables = $tablesCreator->createTables();
+    $tables ? print 'Created Tables' : print 'Error in query create table';
+
+    $descTables = $tablesCreator->getTables();
+    print '<pre>';
+    print_r($descTables);
+    print '</pre>';
+
+    // sleep(3);
+    $tempo = time() - $_SERVER["REQUEST_TIME_FLOAT"];
+    printf("%0.16f segs", $tempo/1000000);
+
+    print "<br>";
+    print time() - $_SERVER["REQUEST_TIME_FLOAT"] / 1000000;
+}
+catch (\PDOException $e) {
+    print $e->getMessage();
+}
+
+print "<br><br>";
+foreach (file('queries-postgres/00-criacao-da-database.sql') as $ln) {
+    print $ln . "<br>";
 }
---
+++ postgres/Model/CreateDatabase.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace App\Model;
+
+/**
+ * Create table in PostgreSQL from PHP demo
+ */
+class CreateDatabase
+{
+    /**
+     * PDO object
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * init the object with a \PDO object
+     * @param type $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * create database 
+     */
+    public function createDB()
+    {
+        /**
+         * queries PostgreSQL
+         *      $ DROP DATABASE loja;
+         *      $ CREATE DATABASE loja;
+         */
+        $sqlQuery = [
+            // 'DROP DATABASE loja;',
+            'CREATE DATABASE loja;'
+        ];
+
+        // execute each sql statement to create new database
+        foreach ($sqlQuery as $sql) {
+            $this->pdo->exec($sql);
+        }
+
+        return $this;
+    }
+}
