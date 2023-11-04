147a78b5a0e71464e561851fb4ff85e19d47a4ce
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-11-04 17:20:14 -0300

    Adicionando comentários.

--- queries-postgres/section-30/123-4-criando-db-projeto.sql
+++ queries-postgres/section-30/123-4-criando-db-projeto.sql
@@ -1,3 +1,7 @@
+/*
+    A base de dados foi criada com scripts PHP
+    Veja nos diretórios 'postgres', 'postgres/Model'
+*/
 SELECT * FROM departamentos;
 SELECT * FROM localizacao;
 SELECT * FROM funcionarios;
