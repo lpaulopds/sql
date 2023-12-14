9afd887b22836fade4fdb97df86e1034845f2c92
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-14 19:16:33 -0300

    Organizando arquivos e scripts com diretórios.

---
+++ postgres/scripts-conf/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'postgres';
+$port = 5432;
+$db = 'loja';
+$user = 'postgres';
+$password = 'postgres';
---
+++ postgres/scripts-init/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ls -la ~/
+
+# apt-get update && \
+# apt-get upgrade -y && \
+# apt-get install -y git && \
+# apt-get install -y nano && \
+# apt-get install -y net-tools && \
+# apt-get install -y unzip && \
+# apt-get install -y gzip && \
+# apt-get install -y apt-transport-https && \
+# apt-get install -y libicu-dev && \
+# apt-get install -y icu-devtools && \
+# apt-get install -y curl && \
+# apt-get install -y openssl
+# apt-get install -y build-essential && \
+# apt-get install -y locales
