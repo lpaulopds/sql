48e03de4198492765ddedca2063670d0d8ca64b8
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-16 19:49:55 -0300

    Removing Warnings on install and updates rpm packages.

--- mysql/scripts-init/entrypoint.sh
+++ mysql/scripts-init/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-rpm -ivh rpm-packages/rpm-4.14.3-26.el8.src.rpm
-exec /usr/sbin/mysqld
+groupadd mock
+useradd mockbuild -g mock
 
+rpm -ivh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
+rpm -ivh rpm-packages/rpm-4.14.3-26.el8.src.rpm
+
+rpm -Uvh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
+rpm -Uvh rpm-packages/rpm-4.14.3-26.el8.src.rpm
+
+exec /usr/sbin/mysqld
