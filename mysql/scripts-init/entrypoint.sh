b1a8d7a03bc9b7bb35265e98db553248101963cd
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-19 00:48:58 -0300

    Otimized entrypoint.sh file.

--- mysql/scripts-init/entrypoint.sh
+++ mysql/scripts-init/entrypoint.sh
@@ -10,8 +10,8 @@ useradd mockbuild -g mock
 ### Download pacotes rpm
 ##
 curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/nano-2.9.8-1.el8.x86_64.rpm
-curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/bash-4.4.20-4.el8_6.x86_64.rpm
-curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/rpm-4.14.3-26.el8.x86_64.rpm
+# curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/bash-4.4.20-4.el8_6.x86_64.rpm
+# curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/rpm-4.14.3-26.el8.x86_64.rpm
 
 ##
 ### Move rpm packages para diretório definido
@@ -22,18 +22,19 @@ mv *.rpm rpm-packages/
 ##
 ### Instala rpm packages manualmente
 ##
-rpm -ivh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
-rpm -ivh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
+# rpm -ivh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
+# rpm -ivh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
 rpm -ivh rpm-packages/nano-2.9.8-1.el8.x86_64.rpm
 
 ##
 ### Atualiza rpm packages recém instalados
 ##
-rpm -Uvh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
-rpm -Uvh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
+# rpm -Uvh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
+# rpm -Uvh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
 rpm -Uvh rpm-packages/nano-2.9.8-1.el8.x86_64.rpm
 
 ##
 ### Inicia container
 ##
+chmod 1770 /var/run/mysqld/
 exec /usr/sbin/mysqld
