af2a64e37f64dd42d1f04223893ee305b4ac9691
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-19 17:00:30 -0300

    Install YUM rpm package on MySQL container.

--- mysql/scripts-init/entrypoint.sh
+++ mysql/scripts-init/entrypoint.sh
@@ -10,6 +10,7 @@ useradd mockbuild -g mock
 ### Download pacotes rpm
 ##
 curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/nano-2.9.8-1.el8.x86_64.rpm
+curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/yum-4.7.0-16.0.1.el8_8.noarch.rpm
 # curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/bash-4.4.20-4.el8_6.x86_64.rpm
 # curl -O https://yum.oracle.com/repo/OracleLinux/OL8/8/baseos/base/x86_64/getPackage/rpm-4.14.3-26.el8.x86_64.rpm
 
@@ -25,6 +26,7 @@ mv *.rpm rpm-packages/
 # rpm -ivh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
 # rpm -ivh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
 rpm -ivh rpm-packages/nano-2.9.8-1.el8.x86_64.rpm
+rpm -ivh rpm-packages/yum-4.7.0-16.0.1.el8_8.noarch.rpm
 
 ##
 ### Atualiza rpm packages recém instalados
@@ -32,6 +34,7 @@ rpm -ivh rpm-packages/nano-2.9.8-1.el8.x86_64.rpm
 # rpm -Uvh rpm-packages/rpm-4.14.3-26.el8.x86_64.rpm
 # rpm -Uvh rpm-packages/bash-4.4.20-4.el8_6.x86_64.rpm
 rpm -Uvh rpm-packages/nano-2.9.8-1.el8.x86_64.rpm
+rpm -Uvh rpm-packages/yum-4.7.0-16.0.1.el8_8.noarch.rpm
 
 ##
 ### Inicia container
