f59d1500c459cbeb6d8f63ef36fc91b87fb9c4c6
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-19 16:03:51 -0300

    Old config commented.

--- mssql/entrypoint.sh
+++ mssql/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
-set -m
-/opt/mssql/bin/sqlservr & ./run-initialization.sh
-fg
+
+# set -m
+# /opt/mssql/bin/sqlservr & ./run-initialization.sh
+# fg
+
+##
+### Espera carregamento do sistema
+##
+sleep 20
+
+##
+### Atualiza e instala apt packages
+##
+# apt-get update
+
+##
+### Inicia container
+##
+/opt/mssql/bin/sqlservr 
