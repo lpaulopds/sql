322768d1844b4ce2c60e984457412024389ecc2c
Luiz Paulo <luizpaulopds@outlook.com.br>
2023-12-25 17:11:17 -0300

    Otimization in entry point file. POSTGRES container.

--- postgres/scripts-init/entrypoint.sh
+++ postgres/scripts-init/entrypoint.sh
@@ -6,24 +6,6 @@
 chmod  0750 /var/lib/postgresql/data/
 
 ##
-### Instala apt packages
-##
-# apt-get update 
-# apt-get upgrade -y && \
-# apt-get install -y git && \
-# apt-get install -y nano && \
-# apt-get install -y net-tools && \
-# apt-get install -y unzip && \
-# apt-get install -y gzip && \
-# apt-get install -y apt-transport-https && \
-# apt-get install -y libicu-dev && \
-# apt-get install -y icu-devtools && \
-# apt-get install -y curl && \
-# apt-get install -y openssl
-# apt-get install -y build-essential && \
-# apt-get install -y locales
-
-##
 ### Inicia container definindo usuário seguro
 ##
 su -c "exec /usr/lib/postgresql/16/bin/postgres" postgres
