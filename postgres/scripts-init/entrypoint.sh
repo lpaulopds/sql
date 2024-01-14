28cbe5e2c628a3bb5f95a2a84432a139d7579ced
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-01-14 19:12:29 -0300

    adding documentation and removing redundant command

--- postgres/scripts-init/entrypoint.sh
+++ postgres/scripts-init/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+#
+## Atenção!
+## No primeiro Build do container, executar sem o padrão
+## entry point, para isso, comente a linha
+## do Dockerfile com o comando [CMD]. Dessa maneira
+## todos os arquivos do database serão criados corretamente.
+#
+
 ##
 ### Permissão padrão
 ##
-chmod  0750 /var/lib/postgresql/data/
+chmod 0750 /var/lib/postgresql/data/
 
 ##
 ### Inicia container definindo usuário seguro
