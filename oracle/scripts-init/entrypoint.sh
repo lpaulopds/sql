43d50e5cd27d68ad34ac6d6356a234a30a2cbce6
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-01-21 00:39:34 -0300

    [Oracle ENTRYPOINT] Container administration commands

--- oracle/scripts-init/entrypoint.sh
+++ oracle/scripts-init/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+cp /home/oracle/sql/*.ora $ORACLE_HOME/network/admin/samples
+chown oracle:oinstall $ORACLE_HOME/network/admin/samples/*.ora
+# su -c "$ORACLE_HOME/bin/lsnrctl start" oracle
+
 ##
 ### Inicia container
 ##
