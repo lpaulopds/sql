4a4c023740080eb4e646a4afe6e1fc6c0b309d8f
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-01-21 23:01:15 -0300

    [Oracle ENTRYPOINT] Small documentation at the biginning of the file

--- oracle/scripts-init/entrypoint.sh
+++ oracle/scripts-init/entrypoint.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
+#### Iniciando com Oracle Database 19c
+#### Execute o sqlplus com o usuário oracle
+##   $ su oracle
+
+#### Conecte ao database com nolog 
+##   $ $ORACLE_HOME/bin/sqlplus /nolog
+
+#### Conecte ao banco com o usuário sysdba como alias
+##   $ connect / as sysdba
+##   $ connect sys/sys as sysdba
+
+#### Inicia o database
+##   $ startup
+##   $ shutdown abort
+
+## $ show pdbs
+## $ lsnrctl status
+
+
 cp /home/oracle/sql/*.ora $ORACLE_HOME/network/admin/samples
 chown oracle:oinstall $ORACLE_HOME/network/admin/samples/*.ora
-# su -c "$ORACLE_HOME/bin/lsnrctl start" oracle
 
 ##
 ### Inicia container
