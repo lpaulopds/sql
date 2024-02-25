be2874c4ff27543e12d58deff52b5a7e0ed6d687
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-02-25 00:31:30 -0300

    [PostgreSQL ENTRYPOINT] Otimized

--- postgres/scripts-init/entrypoint.sh
+++ postgres/scripts-init/entrypoint.sh
@@ -8,12 +8,8 @@
 ## todos os arquivos do database serão criados corretamente.
 #
 
-##
 ### Permissão padrão
-##
 chmod 0750 /var/lib/postgresql/data/
 
-##
 ### Inicia container definindo usuário seguro
-##
 su -c "exec /usr/lib/postgresql/16/bin/postgres" postgres
