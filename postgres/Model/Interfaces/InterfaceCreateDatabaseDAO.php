ac6f6cc1fc9a202ca946d321156028de263fefe4
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-02-29 04:07:52 -0300

    Interfaces DAO to create database and create tables

---
+++ postgres/Model/Interfaces/InterfaceCreateDatabaseDAO.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace App\Model\Interfaces;
+
+interface InterfaceCreateDatabaseDAO {
+    public function createDB();
+}
---
+++ postgres/Model/Interfaces/InterfaceCreateTablesDAO.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\Model\Interfaces;
+
+interface InterfaceCreateTablesDAO
+{
+    public function createTablesLoja();
+    public function createTablesLocadora();
+}
