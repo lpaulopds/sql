f622114a9e6e70ae223d7421be3269db674415e7
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-07 17:18:45 -0300

    [PostgreSQL] Developing routes

--- index.php
+++ index.php
@@ -5,25 +5,42 @@
 
 require 'vendor/autoload.php';
 
+use \App\Http\Request;
+
+$obRequest = new Request;
+echo "<pre>";
+print_r($obRequest);
+// print_r($obRequest->getHeaders()['Host']);
+// print_r($obRequest->getHttpMethod());
+// print_r($obRequest->getUri());
+echo "</pre>";
+
+die;
+
+
 use \App\Http\Router;
+// use \App\Http\Request;
 
 $obRouter = new Router();
+$obRequest = new Request();
 
 // print_r($obRouter);die;
 
 $obRouter->addRoute('GET', '/' , function()
 {
     echo "<pre>";
-    print_r($_SERVER);
+    // print_r($_SERVER);
+    print_r(getallheaders());
     echo "</pre>";
-    phpinfo();
+    // phpinfo();
 });
 
 $obRouter->addRoute('GET', '/postgres/' , function() {
     require 'postgres-php-scripts/index.php';
 });
 
-$method = $_SERVER['REQUEST_METHOD'];
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $method = $_SERVER['REQUEST_METHOD'];
+// $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $obRouter->dispatch($method, $path);
 
-$obRouter->dispatch($method, $path);
+$obRouter->dispatch($obRequest->getHttpMethod(), $obRequest->getUri());
