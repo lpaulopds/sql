8f9f093ef64059af05f6f44c2e01a92971cde5df
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-07 02:05:11 -0300

    [PostgreSQL] Adding Router class

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<?php
+<?php declare(strict_types=1);
 
 // // GPG key routine for github
 // // GPG_TTY=$(tty) && export GPG_TTY && eval $(ssh-agent -s)
 
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-phpinfo();
-die;
+require 'vendor/autoload.php';
+
+use \App\Http\Router;
+
+$obRouter = new Router();
+
+// print_r($obRouter);die;
+
+$obRouter->addRoute('GET', '/' , function()
+{
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
+    phpinfo();
+});
+
+$obRouter->addRoute('GET', '/postgres/' , function() {
+    require 'postgres-php-scripts/index.php';
+});
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$obRouter->dispatch($method, $path);
