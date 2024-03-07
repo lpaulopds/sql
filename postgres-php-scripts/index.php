6a43cfe543ef7c6586ca7b4c1915955db3bb4f46
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-07 02:04:30 -0300

    [PostgreSQL] Adding Router class

--- postgres-php-scripts/index.php
+++ postgres-php-scripts/index.php
@@ -2,58 +2,19 @@
 
 $time_start = microtime(true);
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
-// use \App\Controller\DatabaseCreator;
-// new DatabaseCreator();
-// die;
+/////////////////
+use \App\Controller\DatabaseCreator;
+
 use \App\Controller\CreateBusinessTables;
 use \App\Controller\InsertInTables;
 
-// http_response_code(404);
+// new DatabaseCreator();
 
-if (
-    $_SERVER['REQUEST_METHOD'] === 'GET' &&
-    $_SERVER['REQUEST_URI'] === '/postgres-php-scripts/index.php' &&
-    $_SERVER['REQUEST_SCHEME'] === 'http' )
-{
-    new CreateBusinessTables();
-    new InsertInTables();
-}
-else {
-    die('Not found');
-}
-
-
-// echo '<pre>';
-// print_r(array_keys(getallheaders()));
-// echo '</pre>';
-
-// $keyReqHostHeader = array_keys(getallheaders());
-// print $keyReqHostHeader[0];
-
-// $valReqHostHeader = array_values(getallheaders());
-// print $valReqHostHeader[0];
-
-// header("Accept: text/plan");
-// header("Content-Type: text/plan");
-
-$allRequestHeaders = getallheaders();
-foreach ($allRequestHeaders as $header => $values)
-{
-    print "$header: $values <br>";
-}
-
-
-echo '<pre>';
-print_r(getallheaders());
-echo '</pre>';
-
-// header();
-
-// http_response_code(404);
-// print_r($_SERVER['REQUEST_SCHEME']);
-// print $_SERVER['REDIRECT_URL'];
+new CreateBusinessTables();
+// new InsertInTables();
+/////////////////
 
 $time_end = microtime(true);
 $time = $time_end - $time_start;
