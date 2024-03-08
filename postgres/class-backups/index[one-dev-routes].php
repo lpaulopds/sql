a5a38dbf17d208911011666e90960b64717d0c80
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-08 02:23:03 -0300

    Backup postgres index

---
+++ postgres/class-backups/index[one-dev-routes].php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+// // GPG key routine for github
+// // GPG_TTY=$(tty) && export GPG_TTY && eval $(ssh-agent -s)
+
+require 'vendor/autoload.php';
+
+// use \App\Http\Request;
+
+// $obRequest = new Request;
+// echo "<pre>";
+// print_r($obRequest);
+// // print_r($obRequest->getHeaders()['Host']);
+// // print_r($obRequest->getHttpMethod());
+// // print_r($obRequest->getUri());
+// echo "</pre>";
+
+// die;
+
+
+use \App\Http\Router;
+// use \App\Http\Request;
+
+$obRouter = new Router();
+// $obRequest = new Request();
+$request = $obRouter->requestRouter();
+
+// echo "<pre>";
+// // print_r($request);
+// print_r($obRouter);
+// echo "</pre>";die;
+
+$obRouter->addRoute('GET', '/' , function()
+{
+    echo "<pre>";
+    // print_r($_SERVER);
+    print_r(getallheaders());
+    echo "</pre>";
+    // phpinfo();
+});
+
+$obRouter->addRoute('GET', '/postgres/', function() {
+    require 'postgres-php-scripts/index.php';
+});
+
+// $method = $_SERVER['REQUEST_METHOD'];
+// $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $obRouter->dispatch($method, $path);
+
+$obRouter->dispatch($request->getHttpMethod(), $request->getUri());
