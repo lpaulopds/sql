e27c9a265f0ae3da5afc37bd7b6d4153dbe64732
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-08 02:28:49 -0300

    Adding InfraBusiness class for database controller

---
+++ postgres/Controller/InfraBusiness.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use \App\Controller\DatabaseCreator;
+use \App\Controller\CreateBusinessTables;
+use \App\Controller\InsertInTables;
+
+class InfraBusiness 
+{
+    /**
+     * @var DatabaseCreator
+     */
+    private $dataCrea;
+
+    /**
+     * @var CreateBusinessTables
+     */
+    private $creaBusiTabl;
+    
+    /**
+     * @var InsertInTables
+     */
+    private $inseTabl;
+
+    public function __construct()
+    {
+        // $this->dataCrea = new DatabaseCreator();
+        $this->creaBusiTabl = new CreateBusinessTables();
+        // $this->inseTabl = new InsertInTables();
+    }
+
+    public function getDatabaseCreator() {
+        return $this->dataCrea;
+    }
+
+    public function getCreateBusiTables() {
+        return $this->creaBusiTabl;
+    }
+
+    public function getInsertInTables() {
+        return $this->inseTabl;
+    }
+}
