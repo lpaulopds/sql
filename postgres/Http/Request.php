cd523e6cb3995f224a8acc273f4c9f76c22aaa94
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-10 01:54:24 -0300

    [PostgreSQL] Adding post vars method in Router class

--- postgres/Http/Request.php
+++ postgres/Http/Request.php
@@ -10,12 +10,15 @@ class Request
 
     private $headers = [];
 
+    private $postVars = [];
+
     public function __construct()
     {
         $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
         $this->uri = $_SERVER['REQUEST_URI'] ?? '';
         // $this->setUri();
         $this->headers = getallheaders();
+        $this->setPostVars();
     }
 
     public function getHttpMethod() {
@@ -35,5 +38,21 @@ class Request
         // // remove parametros da uri
         $remoParaURI = explode('?', $this->uri);
         $this->uri = $remoParaURI[0];
+    }
+
+    public function getPostVars() {
+        return $this->postVars;
+    }
+
+    private function setPostVars()
+    {
+        if ($this->httpMethod == 'GET') return false;
+
+        $this->postVars = $_POST ?? [];
+
+        $inputRaw = file_get_contents('php://input');
+        $this->postVars = (strlen($inputRaw)) && empty($_POST) ?
+            json_decode($inputRaw, true) :
+                $this->postVars;
     }
 }
