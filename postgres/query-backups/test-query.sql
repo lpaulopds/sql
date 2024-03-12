136d88e7fdb40423ac7d20e540c8718be5f2cdb2
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-12 03:02:30 -0300

    [PostgreSQL] Adding file to test sql query

---
+++ postgres/query-backups/test-query.sql
@@ -0,0 +1,6 @@
+SELECT f.nome, f.email, f.departamento, f.cargo, l.localizacao
+    FROM funcionarios f
+        INNER JOIN localizacao l
+        ON f.idregiao = l.idregiao
+            WHERE nome ILIKE 'kelley'
+            OR email ILIKE 'rkelley0@soundcloud.com';
