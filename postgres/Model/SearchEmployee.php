d60954d565670027135090c4cb8ae48df661327b
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-16 00:28:25 -0300

    [PostgreSQL] Changing to

--- postgres/Model/SearchEmployee.php
+++ postgres/Model/SearchEmployee.php
@@ -1,9 +1,26 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types=1); // Strategy Search Employee
 
 namespace App\Model;
 
-class SearchEmployee
+use App\Model\Interfaces\IStrategyStore;
+
+class SearchEmployee implements IStrategyStore
 {
+    /**
+     * @var string
+     */
+    private $tableMasterEmployee;
+
+    /**
+     * @var string
+     */
+    private $tableMasterLocalization;
+
+    /**
+     * @var array
+     */
+    private $dataPack;
+
     /**
      * @var \PDO
      */
@@ -19,34 +36,28 @@ class SearchEmployee
      */
     private $stmt;
 
-    /**
-     * @var string
-     */
-    public $searchTerm;
-
     public function __construct($pdo)
     {
         $this->pdo = $pdo;        
     }
 
-    public function getSearchEmployee() {
-        return $this->setSearchEmployee();
-    }
-
-    private function setSearchEmployee()
+    public function algorithm(Array $dataPack)
     {
-        $this->sql = 'SELECT f.nome, f.email, f.departamento, f.cargo, l.localizacao
-                        FROM funcionarios f
-                            INNER JOIN localizacao l
+        $this->dataPack = $dataPack;
+        $this->tableMasterEmployee = IStrategyStore::TABLEEMPLOYEE;
+        $this->tableMasterLocalization = IStrategyStore::TABLELOCALIZATION;
+
+        $this->sql = "SELECT f.nome, f.email, f.departamento, f.cargo, l.localizacao
+                        FROM $this->tableMasterEmployee f
+                            INNER JOIN $this->tableMasterLocalization l
                             ON f.idregiao = l.idregiao
                                 WHERE nome ILIKE :searchTerm
-                                OR email ILIKE :searchTerm';
+                                OR email ILIKE :searchTerm";
 
         $this->stmt = $this->pdo->prepare($this->sql);
+        $this->stmt->bindValue(':searchTerm', $this->dataPack['search']);
 
-        $this->stmt->bindValue(':searchTerm', $this->searchTerm);
-        $this->stmt->execute();
-
+        $this->stmt->execute() ?? throw new \PDOException("Error Processing Request", 1);
         return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 }
