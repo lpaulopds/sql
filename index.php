e568b11b73238de9455b04eee7fb96982da7684b
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-29 21:34:56 -0300

    [PostgreSQL] Developing Logs

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ $time_start = microtime(true);
 require 'vendor/autoload.php';
 
 use App\Controller\ClientStrategyStore;
-use \App\Http\Router;
-use \App\Http\Response;
-use \App\Controller\InfraBusiness;
+use App\Http\Router;
+use App\Http\Response;
+use App\Controller\InfraBusiness;
 
 $obRouter = new Router();
 
@@ -52,6 +52,23 @@ function()
     $obSearch = new ClientStrategyStore();
     return new Response(200, $obSearch->searchEmployeeStore());
 });
+
+// // Função de tratamento de erro personalizada
+// function handleError($errno, $errstr, $errfile, $errline) {
+//     echo "Erro: [$errno] $errstr - $errfile:$errline";
+//     // print_r($errcontext[0]);
+//     // Você pode fazer mais coisas aqui, como registrar o erro em um arquivo de log
+// }
+
+// // Define a função de tratamento de erro personalizada
+// set_error_handler("handleError");
+
+function handleError($errno, $errstr, $errfile, $errline)
+{
+    echo "<h1>[$errno] $errstr </h1>";
+    echo "<h3>$errfile $errline</h3>";
+}
+set_error_handler("handleError");
 
 
 $obRequest = $obRouter->requestRouter();
