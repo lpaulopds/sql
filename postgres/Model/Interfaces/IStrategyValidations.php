e28fb12d87b80f2487c689b0506ed699b0ae6262
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-29 21:30:22 -0300

    [PostgreSQL] Developing validations

--- postgres/Model/Interfaces/IStrategyValidations.php
+++ postgres/Model/Interfaces/IStrategyValidations.php
@@ -5,4 +5,6 @@ namespace App\Model\Interfaces;
 interface IStrategyValidations {
     public static function validateSearch($data): bool;
     public static function validateEmployeeName($data): bool;
+    public static function validateEmployeeEmail($data): bool;
+    public static function validateEmployeeSexo($data): bool;
 }
