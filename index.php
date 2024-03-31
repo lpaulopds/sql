6808dcef9778442e45b9f6c26862429c6400a13b
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-03-31 09:41:15 -0300

    [PostgreSQL] Adding error class

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ use App\Controller\ClientStrategyStore;
 use App\Http\Router;
 use App\Http\Response;
 use App\Controller\InfraBusiness;
+use App\Controller\ClientStrategyError;
 
 $obRouter = new Router();
 
@@ -53,23 +54,9 @@ function()
     return new Response(200, $obSearch->searchEmployeeStore());
 });
 
-// // Função de tratamento de erro personalizada
-// function handleError($errno, $errstr, $errfile, $errline) {
-//     echo "Erro: [$errno] $errstr - $errfile:$errline";
-//     // print_r($errcontext[0]);
-//     // Você pode fazer mais coisas aqui, como registrar o erro em um arquivo de log
-// }
 
-// // Define a função de tratamento de erro personalizada
-// set_error_handler("handleError");
-
-function handleError($errno, $errstr, $errfile, $errline)
-{
-    echo "<h1>[$errno] $errstr </h1>";
-    echo "<h3>$errfile $errline</h3>";
-}
-set_error_handler("handleError");
-
+// // Mostra e salva log de erro
+ClientStrategyError::displayError();
 
 $obRequest = $obRouter->requestRouter();
 $obRouter->dispatch($obRequest->getHttpMethod(), $obRequest->getUri());
