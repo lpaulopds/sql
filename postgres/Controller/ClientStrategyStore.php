03c5098324b48b73f3afb3165f37d2578b832c9f
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-04-08 19:04:13 -0300

    [PostgreSQL] Changing class paths

--- postgres/Controller/ClientStrategyStore.php
+++ postgres/Controller/ClientStrategyStore.php
@@ -3,134 +3,189 @@
 namespace App\Controller;
 
 use App\Model\ContextStrategyStore as Context;
-use App\Model\DataEntryDepartmentStore as InsertDepart;
-use App\Model\DataEntryLocalizationStore as InsertLocalization;
-use App\Model\DataEntryEmployeeStore as InsertEmployee;
-use App\Model\DataEntryMachineStore as InsertMachine;
-use App\Model\DataEntryGenderStore as InsertGender;
-use App\Model\DataEntryFilmStore as InsertFilm;
-use App\Model\DataEntryLocationStore as InsertLocation;
+use App\Model\Insert\DataEntryDepartmentStore as InsertDepart;
+use App\Model\Insert\DataEntryLocalizationStore as InsertLocalization;
+use App\Model\Insert\DataEntryEmployeeStore as InsertEmployee;
+use App\Model\Insert\DataEntryMachineStore as InsertMachine;
+use App\Model\Insert\DataEntryGenderStore as InsertGender;
+use App\Model\Insert\DataEntryFilmStore as InsertFilm;
+use App\Model\Insert\DataEntryLocationStore as InsertLocation;
+use App\Model\SelectMenuDepart as SelectDepart;
 
 use App\Model\SearchEmployee as Search;
 use App\Model\InsertEmployeeFormStore as InsertEmployeeF;
 
 class ClientStrategyStore
 {
+    /**
+     * @var object
+     */
+    private $secure;
+
+    /**
+     * @var object
+     */
+    private $context;
+
+    /**
+     * @var string
+     */
+    private $departaments;
+
+    /**
+     * @var string
+     */
+    private $localization;
+
+    /**
+     * @var string
+     */
+    private $employee;
+
+    /**
+     * @var string
+     */
+    private $machine;
+
+    /**
+     * @var string
+     */
+    private $gender;
+
+    /**
+     * @var string
+     */
+    private $film;
+
+    /**
+     * @var string
+     */
+    private $location;
+
     public function insertDataDepartStore()
-    {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertDepart());
-        foreach ($secure->enterDataDepartment() as $value)
+    { 
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertDepart);
+        foreach ($this->secure->enterDataDepartment() as $this->departaments)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->departaments = explode(",", $this->departaments);
+            $this->context->contextAlgorithm($this->departaments);
             echo '<pre>';
-            print_r($value);
+                print_r($this->departaments);
             echo '</pre>';
         }
     }
 
     public function insertDataLocalizationStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertLocalization());
-        foreach ($secure->enterDataLocalization() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertLocalization());
+        foreach ($this->secure->enterDataLocalization() as $this->localization)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->localization = explode(",", $this->localization);
+            $this->context->contextAlgorithm($this->localization);
             echo '<pre>';
-            print_r($value);
+                print_r($this->localization);
             echo '</pre>';
         }
     }
 
     public function insertDataEmployeeStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertEmployee());
-        foreach ($secure->enterDataEmployee() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertEmployee());
+        foreach ($this->secure->enterDataEmployee() as $this->employee)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->employee = explode(",", $this->employee);
+            $this->context->contextAlgorithm($this->employee);
             echo '<pre>';
-            print_r($value);
+                print_r($this->employee);
             echo '</pre>';
         }
     }
 
     public function insertDataMachinesStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertMachine());
-        foreach ($secure->enterDataMachines() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertMachine());
+        foreach ($this->secure->enterDataMachines() as $this->machine)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->machine = explode(",", $this->machine);
+            $this->context->contextAlgorithm($this->machine);
             echo '<pre>';
-            print_r($value);
+                print_r($this->machine);
             echo '</pre>';
         }
     }
 
     public function insertDataGenderStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertGender());
-        foreach ($secure->enterDataGender() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertGender());
+        foreach ($this->secure->enterDataGender() as $this->gender)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->gender = explode(",", $this->gender);
+            $this->context->contextAlgorithm($this->gender);
             echo '<pre>';
-            print_r($value);
+                print_r($this->gender);
             echo '</pre>';
         }
     }
 
     public function insertDataFilmStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertFilm());
-        foreach ($secure->enterDataFilm() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertFilm());
+        foreach ($this->secure->enterDataFilm() as $this->film)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->film = explode(",", $this->film);
+            $this->context->contextAlgorithm($this->film);
             echo '<pre>';
-            print_r($value);
+                print_r($this->film);
             echo '</pre>';
         }
     }
 
     public function insertDataLocationStore()
     {
-        $secure = new SecureDataStore();
-        $context = new Context(new InsertLocation());
-        foreach ($secure->enterDataLocation() as $value)
+        $this->secure = new SecureDataStore();
+        $this->context = new Context(new InsertLocation());
+        foreach ($this->secure->enterDataLocation() as $this->location)
         {
-            $value = explode(",", $value);
-            $context->contextAlgorithm($value);
+            $this->location = explode(",", $this->location);
+            $this->context->contextAlgorithm($this->location);
             echo '<pre>';
-            print_r($value);
+                print_r($this->location);
             echo '</pre>';
         }
     }
 
     public function searchEmployeeStore()
     {
-        $secure = new SearchEmployeeStore();
-        $context = new Context(new Search());
-        print '<pre>';
-        print_r($secure->getSearchEmployee());
-        print '</pre>';
-        $context->contextAlgorithm($secure->setEntry());
+        $this->secure = new SearchEmployeeStore();
+        $this->context = new Context(new Search());
+        echo '<pre>';
+            print_r($this->secure->getSearchEmployee());
+        echo '</pre>';
+        $this->context->contextAlgorithm($this->secure->setEntry());
+    }
+
+    public function selectMenuDepart()
+    {
+        $selectDepart = new SelectDepartEmployee();
+        $this->context = new Context(new SelectDepart);
+        $sel = $selectDepart->getSelect();
+        $this->context->contextAlgorithm(array(0));
+        return $sel;
     }
 
     public function insertEmployeeFormStore()
     {
-        $secure = new InsertEmployeeStore();
-        $context = new Context(new InsertEmployeeF());
-        print '<pre>';
-        print_r($secure->getInsertEmployee());
-        print '</pre>';
-        $context->contextAlgorithm($secure->setEntry());
+        $this->secure = new InsertEmployeeStore();
+        $this->context = new Context(new InsertEmployeeF());
+        echo '<pre>';
+            print_r($this->secure->getInsertEmployee());
+        echo '</pre>';
+        $this->context->contextAlgorithm($this->secure->setEntry());
     }
 }
