9a77f2bb1c7750bfcea06f7ee1c14b0ce5c4b4e7
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-04-17 14:36:39 -0300

    [PostgreSQL] New query for employee select

--- postgres/query-backups/test-query.sql
+++ postgres/query-backups/test-query.sql
@@ -1,6 +1,13 @@
+-- Query for seach
 SELECT f.nome, f.email, f.departamento, f.cargo, l.localizacao
     FROM funcionarios f
         INNER JOIN localizacao l
-        ON f.idregiao = l.idregiao
-            WHERE nome ILIKE 'kelley'
-            OR email ILIKE 'rkelley0@soundcloud.com';
+            ON f.idregiao = l.idregiao
+                WHERE nome ILIKE 'kelley'
+                    OR email ILIKE 'rkelley0@soundcloud.com';
+
+-- Query for select
+SELECT f.nome, l.localizacao
+    FROM funcionarios f
+        INNER JOIN localizacao l
+            ON f.idregiao = l.idregiao;
