69bd1278ecb606bc3e3b36175809c783332c7aa4
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-04-21 19:13:06 -0300

    [PostgreSQL] Rendering insert form

--- postgres/Controller/EmployeeInsertForm.php
+++ postgres/Controller/EmployeeInsertForm.php
@@ -1,14 +1,78 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types=1); // Strategy - Controller
 
 namespace App\Controller;
 
-use App\Controller\ClientStrategyStore;
+use App\Utils\View; 
+use App\Controller\Pages\Page;
+use App\Model\SelectMenuDepart;
 
-class EmployeeInsertForm
+class EmployeeInsertForm extends Page
 {
-    public static function EmployeeInsertForm()
-    {   
-        $obMenuSelect = new ClientStrategyStore();
-        require 'postgres/resources/view/employee-insert-form.php';
+    /**
+     * @var array
+     */
+    private $select;
+
+    /**
+     * @var SelectMenuDepart
+     */
+    private $obSelect;
+
+    /**
+     * @var array
+     */
+    private $object;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var string
+     */
+    private $departs;
+
+    /**
+     * Instância da classe Model
+     * @return array
+     */
+    private function setSelectMenuDepart()
+    {
+        $this->select = new SelectMenuDepart();
+        return $this->select;
+    }
+
+    /**
+     * Retorna departamentos
+     * @return string
+     */
+    private function setMenuDepart()
+    {
+        $this->obSelect = $this->setSelectMenuDepart();
+        $this->object = $this->obSelect->algorithm(array(0));
+        $this->content = '';
+
+        foreach ($this->object as $this->departs)
+        {
+            $this->content .= View::render('view/menu-select', [
+                'departs' => $this->departs['departamento']
+            ]);
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * Retorna formulário de inserção
+     * @return string
+     */
+    public function EmployeeInsertForm()
+    {
+        $this->content = View::render('view/employee-insert-form', [
+            'menuSelect' => $this->setMenuDepart()
+        ]);
+
+        print parent::getPage('Employee Insert', $this->content);
     }
 }
