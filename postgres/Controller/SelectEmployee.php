a668f5bb7ec4bd0a93cfbcbaf7c961b1c592ba66
Luiz Paulo <luizpaulopds@outlook.com.br>
2024-04-21 18:52:49 -0300

    [PostgreSQL] Documenting and encapsulating variables

--- postgres/Controller/SelectEmployee.php
+++ postgres/Controller/SelectEmployee.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use App\Model\EmployeeSelect;
 use App\Utils\View; 
 use App\Controller\Pages\Page;
+use App\Model\EmployeeSelect;
 
 class SelectEmployee extends Page
 {
@@ -18,50 +18,73 @@ class SelectEmployee extends Page
      */
     private $obSelect;
 
-    public function getSelect() {
-        return $this->setSelect();
-    }
-
     /**
-     * @return string
+     * @var array
      */
-    private function setSelectItens()
-    {
-        $this->obSelect = $this->setEmployeeSelect();
-        $content = '';
-
-        foreach ($this->obSelect->algorithm(array(0)) as $value)
-        {
-            $content .= View::render('view/itens', [
-                'nome' => $value['nome'],
-                'email' => $value['email'],
-                'departamento' => $value['departamento'],
-                'cargo' => $value['cargo'],
-                'localizacao' => $value['localizacao']
-            ]);
-        }
-
-        return $content;
-    }
+    private $object;
 
     /**
-     * @return string
+     * @var string
      */
-    public function setSelect()
-    {
-        $content = View::render('view/employee-table', [
-            'itens' => $this->setSelectItens()
-        ]);
-
-        print parent::getPage('Employee Table', $content);
-    }
+    private $content;
 
     /**
+     * @var string
+     */
+    private $employee;
+
+    /**
+     * Retorna classe Model
      * @return EmployeeSelect
      */
     private function setEmployeeSelect()
     {
         $this->select = new EmployeeSelect();
         return $this->select;
+    }
+
+    /**
+     * Retorna tabela com seleção de funcionários
+     * @return string
+     */
+    public function getSelect() {
+        return $this->setSelect();
+    }
+
+    /**
+     * Retorna dados de funcionário
+     * @return string
+     */
+    private function setSelectItens()
+    {
+        $this->obSelect = $this->setEmployeeSelect();
+        $this->object = $this->obSelect->algorithm(array(0));
+        $this->content = '';
+
+        foreach ($this->object as $this->employee)
+        {
+            $this->content .= View::render('view/itens', [
+                'nome' => $this->employee['nome'],
+                'email' => $this->employee['email'],
+                'departamento' => $this->employee['departamento'],
+                'cargo' => $this->employee['cargo'],
+                'localizacao' => $this->employee['localizacao']
+            ]);
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * Retorna tabela com dados de funcionário
+     * @return string
+     */
+    public function setSelect()
+    {
+        $this->content = View::render('view/employee-table', [
+            'itens' => $this->setSelectItens()
+        ]);
+
+        print parent::getPage('Employee Table', $this->content);
     }
 }
